bf3eaaaaccae6f4c76d9ea0c65ef89f41ab0bfe1
Viacheslavik <Slavik2203@gmail.com>
2024-12-17 19:09:19 +0300

    ex

--- autotest1sem/ex5/main5.c
+++ autotest1sem/ex5/main5.c
@@ -1,106 +1,62 @@
 #include"stdio.h"
 #include"stdlib.h"
 
-
-void rsort(int a[], int N);
-void swap(int *a, int *b);
-void task5(int *arra, int *arrb, int k);
+void task5(double *a, int n, double *b, int m, FILE*f);
+int cmp(const void*a, const void*b);
 
 int main(void)
 {
-    int x, M=0, N=0, min=0;
-    int *tmparra;
-    int *arra;
-    int *tmparrb;
-    int *arrb;
-
-    FILE*fina = fopen("ina.txt", "r");
-    FILE*finb = fopen("inb.txt", "r");
-    FILE*fout = fopen("output.txt", "w");
-    if(!fina){return -1;}
-    if(!finb){fclose(fina); return -1;}
-    if(!fout){fclose(finb);fclose(fina); return -1;}
-    while (fscanf(fina, "%d", &x)==1){M++;}
-    while (fscanf(finb, "%d", &x)==1){N++;}
-
-    rewind(fina);
-    rewind(finb);
-
-    tmparra = (int*)malloc(M * sizeof(int));
-    arra=tmparra;
-    tmparrb = (int*)malloc(N * sizeof(int));
-    arrb=tmparrb;
-
-    for(int i=0; i<M; i++, arra++)
-    {
-        fscanf(fina, "%d", &x);
-        *arra=x;
-    }
-    for(int i=0; i<N; i++, arrb++)
-    {
-        fscanf(finb, "%d", &x);
-        *arrb=x;
-    }
-    arra=tmparra;
-    arrb=tmparrb;
-
-    rsort(arra, M);
-    rsort(arrb, N);
-
-    arra=tmparra;
-    arrb=tmparrb;
-
-    min = (M<=N) ? M : N;
-
-    task5(arra, arrb, min);
-
-    arra=tmparra;
-    arrb=tmparrb;
-
-    fprintf(fout, "%d %d", *arra, *arrb);
-
-    free(tmparra);
-    free(tmparrb);
-    fclose(fina); 
-    fclose(finb); 
+    int sizeA=0, sizeB=0;
+    double *a, *b, x;
+    FILE* finA = fopen("ina.txt", "r");
+    FILE* finB = fopen("inb.txt", "r");
+    FILE* fout = fopen("output.txt", "w");
+    if(!finA){return -1;}
+    if(!finB){fclose(finA); return -1;}
+    if(!fout){fclose(finA);fclose(finB); return -1;}
+    while(fscanf(finA, "%lf", &x)==1){sizeA++;}
+    while(fscanf(finB, "%lf", &x)==1){sizeB++;}
+    rewind(finA);
+    rewind(finB);
+    if(sizeA==0||sizeB==0){fclose(finA); fclose(finB); fclose(fout); return -1;}
+    a = (double*)malloc(sizeA * sizeof(double));
+    b = (double*)malloc(sizeB * sizeof(double));
+    for(int i=0; i<sizeA; i++){fscanf(finA, "%lf", &x); a[i]=x;}
+    for(int i=0; i<sizeB; i++){fscanf(finB, "%lf", &x); b[i]=x;}
+    qsort(a, sizeA, sizeof(double), cmp);
+    qsort(b, sizeB, sizeof(double), cmp);
+    task5(a, sizeA, b, sizeB, fout);
+    free(a);
+    free(b);
+    fclose(finA); 
+    fclose(finB); 
     fclose(fout);
-    return 0;
 }
 
-void rsort(int a[], int N)
+int cmp(const void*a, const void*b)
 {
-    for(int i=1; i<N; i++)
+    const double A = *(const double*)a;
+    const double B = *(const double*)b;
+    if(A<B){return 1;}
+    else if(A>B){return -1;}
+    else {return 0;}
+}
+
+void task5(double *a, int n, double *b, int m, FILE*f)
+{
+    int min = (n<m) ? n : m;
+    int count1=0, count2=0;
+    printf("%d", min);
+    for(int i=0; i<min; i++)
     {
-        for(int j=i; j>0 && a[j-1]<a[j]; j--)
-        {
-            swap(&a[j-1], &a[j]);
-        }
-    }
-}
-
-void swap(int *a, int *b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void task5(int *ptr1, int *ptr2, int k)
-{
-    int count1=0, count2=0; 
-    int *arra=ptr1;
-    int *arrb=ptr2;
-    for(int i=0; i<k; i++, arra++, arrb++)
-    {
-        if(*arra<*arrb)
+        if(a[i]<b[i])
         {
             count1++;
         }
-        if(*arra>*arrb)
+        if(a[i]>b[i])
         {
             count2++;
         }
     }
-    *ptr1 = count1;
-    *ptr2 = count2;
+    fprintf(f, "%d %d", count1, count2);
 }
