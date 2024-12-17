386dc47c705355a9e05938e90922606947087df6
Viacheslavik <Slavik2203@gmail.com>
2024-12-17 15:40:43 +0300

    ex3

--- autotest1sem/ex3/main3.c
+++ autotest1sem/ex3/main3.c
@@ -1,108 +1,97 @@
 #include"stdio.h"
 #include"stdlib.h"
 
-void vvod(FILE *fin, int *arr, int k);
-int max(int *arr, int k);
-int min(int *arr, int k);
-int task3(int *arra, int M, int maxA, int *arrb, int N, int minB);
+int cmp(const void*a, const void*b);
+int task3(int *a, int n, int *b, int m);
 
 int main (void)
 {
-    int M=0, N=0, x, maxA, minB, res;
-    int *tmparra;
-    int *arra;
-    int *tmparrb;
-    int *arrb;
+    int n=0, m=0, x, res;
+    int *a;
+    int *b;
     FILE *fina = fopen("ina.txt", "r");
     FILE *finb = fopen("inb.txt", "r");
     FILE *fout = fopen("output.txt", "w");
     if(!fina){return -1;}
     if(!finb){fclose(fina); return -1;}
     if(!fout){fclose(fina); fclose(finb); return -1;}
-    while (fscanf(fina, "%d", &x)==1){M++;}
-    while (fscanf(finb, "%d", &x)==1){N++;}
+    while (fscanf(fina, "%d", &x)==1){n++;}
+    while (fscanf(finb, "%d", &x)==1){m++;}
     rewind(fina);
     rewind(finb);
-    if(M==0 || N==0){fclose(fina); fclose(finb); fclose(fout);return -1;}
-    tmparra = (int*)malloc(M * sizeof(int));
-    arra = tmparra;
-    tmparrb = (int*)malloc(N * sizeof(int));
-    arrb = tmparrb;
-     
-    vvod(fina, arra, M);
-    vvod(finb, arrb, N);
+    if(n==0 || m==0){fclose(fina); fclose(finb); fclose(fout);return -1;}
+    a = (int*)malloc(n * sizeof(int));
+    b = (int*)malloc(m * sizeof(int));
+    
+    for(int i=0; i<n; i++){fscanf(fina, "%d", &x); a[i] = x;}
+    for(int i=0; i<m; i++){fscanf(finb, "%d", &x); b[i] = x;}
 
-    arra = tmparra;
-    arrb = tmparrb;
-
-    maxA = max(arra, M);
-    minB = min(arrb, N);
-
-    arra = tmparra;
-    arrb = tmparrb;
-
-    res = task3(arra, M, maxA, arrb, N, minB);
-
+    qsort(a, n, sizeof(int), cmp);
+    qsort(b, m, sizeof(int), cmp);
+    // for(int i=0; i<n; i++){printf("%d ", a[i]);}
+    // printf("\n");
+    // for(int i=0; i<m; i++){printf("%d ", b[i]);}
+    printf("\n");
+    res = task3(a, n, b, m);
     fprintf(fout, "%d", res);
-    free(tmparra);
-    free(tmparrb);
-    fclose(fina); fclose(finb); fclose(fout);
+    printf("%d", res);
+    free(a);
+    free(b);
+    fclose(fina); 
+    fclose(finb); 
+    fclose(fout);
     return 0;
 }
 
-void vvod(FILE*fin, int *arr, int k)
+int cmp(const void*a, const void*b)
 {
-    int x;
-    for(int i=0; i<k; i++, arr++)
-    {
-        fscanf(fin, "%d", &x);
-        *arr = x;
-    }
+    const int A = *(const int*)a;
+    const int B = *(const int*)b;
+    if(A>B){return 1;}
+    else if(A<B){return -1;}
+    else {return 0;}
 }
 
-int max(int *arr, int k)
+int task3(int *a, int n, int *b, int m)
 {
-    int res=-999;
-    for(int i=0; i<k; i++, arr++)
-    {
-        if(res<*arr)
-        {
-            res = *arr;
-        }
-    }
-    return res;
-}
-int min(int *arr, int k)
-{
-    int res=999;
-    for(int i=0; i<k; i++, arr++)
-    {
-        if(res>*arr)
-        {
-            res = *arr;
-        }
-    }
-    return res;
-}
-
-int task3(int *arra, int M, int maxA, int *arrb, int N, int minB)
-{
+    int minB = b[0];
+    int maxA = a[n-1];
     int res=0;
-    for(int i=0; i<M; i++, arra++)
+    // printf("minB = %d\n", minB);
+    // printf("maxA = %d\n", maxA);
+    if(minB == maxA){return 1;}
+    for(int i=0; i<n; i++)
     {
-        if(*arra>=minB)
+        if(a[i]>=minB)
         {
-            res++;
+            if(i<n-1 && a[i]!=a[i+1]){res++;}
+            if(i==n-1){res++;}
         }
     }
-    for(int i=0; i<N; i++, arrb++)
+    printf("%d\n", res);
+    for(int i=0; i<m; i++)
     {
-        if(*arrb<=maxA)
+        if(b[i]<maxA)
         {
-            res++;
+            if(i<m-1 && b[i]!=b[i+1])
+            {
+                res++;
+                for(int j=0; j<m; j++)
+                {
+                    if(b[i]==a[j]){res--; break;}
+                }
+            }
+            if(i==m-1)
+            {
+                res++;
+                for(int j=0; j<m; j++)
+                {
+                    if(b[i]==a[j]){res--; break;}
+                }
+            }
         }
     }
     return res;
-}
+} //A={1, 1, 2}  B={1, 2}
 
 
